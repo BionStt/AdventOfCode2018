eda7b3ac961d59b8a8d6b0a455c85a8da1fca3ca
Joshua Niederer <josh@jpniederer.com>
2018-12-05 07:46:31 -0600

    Day five.

---
+++ src/DayFive/PairReduce.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2018.DayFive
+{
+    public class PairReduce
+    {
+        public string[] Lines { get; set; }
+        public string Polymer { get; set; }
+
+        public PairReduce() { }
+
+        public PairReduce(string[] lines)
+        {
+            Lines = lines;
+            Polymer = Lines[0];
+        }
+
+        public PairReduce(string filePath)
+        {
+            Lines = System.IO.File.ReadAllLines(filePath);
+            Polymer = Lines[0];
+        }
+
+        public string Reduce()
+        {
+            bool canReduce = true;
+
+            while (canReduce)
+            {
+                bool anyChanges = false;
+
+                for (int i = 0; i < Polymer.Length - 1; i++)
+                {
+                    if (string.Compare(Polymer[i].ToString(), Polymer[i + 1].ToString(), false) != 0
+                        && string.Compare(Polymer[i].ToString(), Polymer[i + 1].ToString(), true) == 0)
+                    {
+                        anyChanges = true;
+                        Polymer = Polymer.Replace((Polymer[i].ToString() + Polymer[i + 1].ToString()), "");
+                        break;
+                    }
+                }
+
+                if (!anyChanges || Polymer.Length < 1)
+                {
+                    canReduce = false;
+                }
+            }
+
+            return Polymer;
+        }
+
+        public int PartTwo()
+        {
+            var characters = Polymer.ToLower()
+                .ToCharArray().Distinct();
+            int min = int.MaxValue;
+
+            foreach (var c in characters)
+            {
+                string trimmed = StripPolymer(c.ToString());
+                string reduced = Reduce(trimmed);
+
+                if (reduced.Length < min)
+                {
+                    min = reduced.Length;
+                }
+            }
+
+            return min;
+        }
+
+        public string StripPolymer(string letter)
+        {
+            return Polymer.Replace(letter, "", true, System.Globalization.CultureInfo.CurrentCulture);
+        }
+
+        public string Reduce(string polymer)
+        {
+            bool canReduce = true;
+
+            while (canReduce)
+            {
+                bool anyChanges = false;
+
+                for (int i = 0; i < polymer.Length - 1; i++)
+                {
+                    if (string.Compare(polymer[i].ToString(), polymer[i + 1].ToString(), false) != 0
+                        && string.Compare(polymer[i].ToString(), polymer[i + 1].ToString(), true) == 0)
+                    {
+                        anyChanges = true;
+                        polymer = polymer.Replace((polymer[i].ToString() + polymer[i + 1].ToString()), "");
+                        break;
+                    }
+                }
+
+                if (!anyChanges || polymer.Length < 1)
+                {
+                    canReduce = false;
+                }
+            }
+
+            return polymer;
+        }
+    }
+}
---
+++ testing/Tests/DayFive.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AdventOfCode2018.DayFive;
+
+namespace Tests
+{
+    public class DayFive
+    {
+        [Fact]
+        public void ExamplePartOne()
+        {
+            var lines = new string[]
+            {
+                "dabAcCaCBAcCcaDA"
+            };
+
+            var pr = new PairReduce(lines);
+            string answer = pr.Reduce();
+
+            Assert.Equal(10, answer.Length);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var pr = new PairReduce(@"C:\Users\joshu\github\AdventOfCode2018\src\DayFive\input.txt");
+            string answer = pr.Reduce();
+
+            Assert.Equal(10368, answer.Length);
+        }
+
+        [Fact]
+        public void ExamplePartTwo()
+        {
+            var lines = new string[]
+            {
+                "dabAcCaCBAcCcaDA"
+            };
+
+            var pr = new PairReduce(lines);
+            int answer = pr.PartTwo();
+
+            Assert.Equal(4, answer);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var pr = new PairReduce(@"C:\Users\joshu\github\AdventOfCode2018\src\DayFive\input.txt");
+            int answer = pr.PartTwo();
+
+            Assert.Equal(4122, answer);
+        }
+    }
+}
